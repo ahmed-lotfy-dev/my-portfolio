de3ecc42e87d2d920c9fca2a62686028d0e18523
ahmed-lotfy-dev <elshenawy19@gmail.com>
2023-01-30 11:45:20 +0200

    finishing styling section contact me responsive

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,23 @@
+import { string } from "zod";
+
 declare module "nodemailer-sendinblue-transport";
 declare module "nodemailer-sendgrid";
 declare module "@sendgrid/mail";
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      GITHUB_AUTH_TOKEN: string;
+      NODE_ENV: "development" | "production";
+      PORT?: string;
+      PWD: string;
+      MONGO_URI: string;
+      BCRYPT_SALT: string;
+      SENDGRID_API_KEY: string;
+      SENDINBLUE_USER: string;
+      SENDINBLUE_APIKEY: string;
+      MAIN_EMAIL: string;
+      SECONDARY_EMAIL: string;
+    }
+  }
+}
--- utils/connectDB.ts
+++ utils/connectDB.ts
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const connectDB = async () => mongoose.connect(process.env.MONGO_URI);
+const connectDB = async () => await mongoose.connect(process.env.MONGO_URI);
 
 export default connectDB;
