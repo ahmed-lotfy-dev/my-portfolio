75b39a5d27729f2e0b0909f642ede21a4f15bc59
ahmed-lotfy-dev <elshenawy19@gmail.com>
2024-01-13 15:12:57 +0200

    fixing images and cloudflare source inside nextjs and links in the db

--- src/app/components/Certificates.tsx
+++ src/app/components/Certificates.tsx
@@ -1,61 +1,62 @@
-import getAllCertificates from "@/lib/getCertificates"
-import Link from "next/link"
-import Image from "next/image"
+import getAllCertificates from "@/lib/getCertificates";
+import Link from "next/link";
+import Image from "next/image";
 
-export const dynamic = "force-dynamic"
+export const dynamic = "force-dynamic";
 
-import { Button } from "./ui/button"
+import { Button } from "./ui/button";
 
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/components/ui/accordion"
+} from "@/components/ui/accordion";
 
 export type Certificate = {
-  id: string
-  certTitle: string
-  certDesc: string
-  certImageLink: string
-  courseLink: string
-  certProfLink: string
-}
+  id: string;
+  certTitle: string;
+  certDesc: string;
+  certImageLink: string;
+  courseLink: string;
+  certProfLink: string;
+};
 
 export default async function certificates() {
-  const { allCertificates } = await getAllCertificates()
-  console.log(allCertificates)
+  const { allCertificates } = await getAllCertificates();
+  console.log(allCertificates);
   return (
-    <section className='bg-blue-200 flex justify-center items-center text-center sm:text-start p-6 '>
-      <div className='container max-w-screen-xl justify-center items-center p-6'>
-        <div className='p-6'>
-          <h2 className='text-3xl font-bold text-center'>Certificates</h2>
+    <section className="bg-blue-200 flex justify-center items-center text-center sm:text-start p-6 ">
+      <div className="container max-w-screen-xl justify-center items-center p-6">
+        <div className="p-6">
+          <h2 className="text-3xl font-bold text-center">Certificates</h2>
         </div>
-        <div className='flex flex-col gap-y-6'>
+        <div className="flex flex-col gap-y-6">
           {allCertificates?.map((cert) => (
             <Accordion
               key={cert.id}
-              className='gap-y-6'
-              type='single'
+              className="gap-y-6"
+              type="single"
               collapsible
             >
               <AccordionItem value={cert.id}>
-                <AccordionTrigger className='font-bold text-1xl'>
+                <AccordionTrigger className="font-bold text-1xl">
                   {cert.certTitle}
                 </AccordionTrigger>
-                <AccordionContent className=''>
+                <AccordionContent className="">
                   <Image
-                    className='m-auto my-6 aspect-auto object-cover'
+                    className="m-auto my-6 aspect-auto object-cover"
                     src={cert.certImageLink}
+                    loading="lazy"
                     width={500}
                     height={500}
                     alt={`${cert.certTitle} certification image`}
                   />
-                  <div className='text-center space-x-5'>
-                    <Link href={cert.courseLink} target='_blank'>
+                  <div className="text-center space-x-5">
+                    <Link href={cert.courseLink} target="_blank">
                       <Button>Course Link</Button>
                     </Link>
-                    <Link href={cert.certProfLink} target='_blank'>
+                    <Link href={cert.certProfLink} target="_blank">
                       <Button>Cerification Proof</Button>
                     </Link>
                   </div>
@@ -66,5 +67,5 @@ export default async function certificates() {
         </div>
       </div>
     </section>
-  )
+  );
 }
