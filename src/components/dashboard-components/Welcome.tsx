285b514a1ce7c5fc2008cc64790266077ba5c335
ahmed-lotfy-dev <elshenawy19@gmail.com>
2024-02-04 22:57:27 +0200

    change welcome message to let user know its role based login cannot interact with action just show

--- src/components/dashboard-components/Welcome.tsx
+++ src/components/dashboard-components/Welcome.tsx
@@ -14,6 +14,14 @@ export default async function Welcome() {
             Welcome {user?.name?.split(" ")[0]} {user?.name?.split(" ")[1]} to
             the dashboard.
           </h2>
+          {user.role === "ADMIN" ? (
+            <p>You are an admin, welcome!</p>
+          ) : (
+            <p>
+              You are not admin . eventhough you can view the site but not
+              interact with action that demand priviliges
+            </p>
+          )}
         </div>
       )}
     </div>
