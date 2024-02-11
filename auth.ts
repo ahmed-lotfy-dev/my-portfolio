43d81bfcdec7a9897013944469f2157ac49c8d8b
ahmed-lotfy-dev <elshenawy19@gmail.com>
2024-02-11 12:24:56 -0500

    next auth route changes

--- auth.ts
+++ auth.ts
@@ -1,11 +1,6 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 
-export const {
-  handlers: { GET, POST },
-  auth,
-  signIn,
-  signOut,
-} = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
 });
