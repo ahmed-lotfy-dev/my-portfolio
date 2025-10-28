b6c8dd49d2694a125ce80e37006f513475e16158
ahmed-lotfy-dev <elshenawy19@gmail.com>
2025-10-28 12:37:18 +0300

    Add more trasnlation, dashoard home

--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -1,47 +1,48 @@
 import { Card } from "@/src/components/ui/card"
 import { getAllCertificates } from "@/src/app/actions/certificatesActions"
 import { getAllProjects } from "@/src/app/actions/projectsActions"
-
 import Welcome from "@/src/components/dashboard-components/Welcome"
 import Link from "next/link"
 import { auth } from "@/src/lib/auth"
 import { headers } from "next/headers"
+import { getTranslations } from "next-intl/server"
 
-export default async function Page({}) {
+export default async function Page() {
   const session = await auth.api.getSession({
     headers: await headers(),
   })
 
   const { allCertificates } = await getAllCertificates()
   const { allProjects } = await getAllProjects()
+
   const projectsCount = allProjects?.length
   const certificatesCount = allCertificates?.length
+
+  // Server translation
+  const t = await getTranslations("dashboard")
+
   return (
     <div className="flex flex-col gap-6 w-full p-5 pt-10">
       <Welcome />
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 justify-start">
         <Link href={"/dashboard/projects"}>
-          <Card className="cursor-pointer p-6 bg-muted hover:bg-accent/30 transition-colors">
+          <Card className="flex flex-col justify-center items-center gap-4 cursor-pointer p-6 bg-muted hover:bg-accent/30 transition-colors">
             <h2 className="text-sm font-medium text-muted-foreground">
-              Projects
+              {t("projects")}
             </h2>
             <p className="text-3xl font-bold">{projectsCount}</p>
           </Card>
         </Link>
+
         <Link href={"/dashboard/certificates"}>
-          <Card className="cursor-pointer p-6 bg-muted hover:bg-accent/30 transition-colors">
+          <Card className="flex flex-col justify-center items-center gap-4 cursor-pointer p-6 bg-muted hover:bg-accent/30 transition-colors">
             <h2 className="text-sm font-medium text-muted-foreground">
-              Certificates
+              {t("certificates")}
             </h2>
             <p className="text-3xl font-bold">{certificatesCount}</p>
           </Card>
         </Link>
       </div>
-      {session?.user?.name && (
-        <div className="mt-2 text-sm text-muted-foreground">
-          <h1>Welcome {session.user.name}</h1>
-        </div>
-      )}
     </div>
   )
 }
--- src/components/dashboard-components/Welcome.tsx
+++ src/components/dashboard-components/Welcome.tsx
@@ -1,20 +1,21 @@
 import { authClient } from "@/src/lib/auth-client"
+import { getTranslations } from "next-intl/server"
 
 export default async function Welcome() {
   const { data: session } = await authClient.getSession()
   const user = session?.user
+  const t = await getTranslations("dashboard.welcome")
+
+  const isAdmin = user?.email === process.env.ADMIN_EMAIL
+
   return (
     <div className="w-full">
       <div className="w-full flex justify-between items-start flex-col pl-10">
-        <h2 className="mb-6">Welcome {user?.name} to the dashboard.</h2>
-        {user?.email === process.env.ADMIN_EMAIL ? (
-          <p>You are admin, welcome!</p>
-        ) : (
-          <p>
-            You are not admin, eventhough you can view the site but not interact
-            with action that demand priviliges
-          </p>
-        )}
+        <h2 className="mb-6">
+          {t("greeting", { name: user?.name || t("guest") })}
+        </h2>
+
+        {isAdmin ? <p>{t("admin_message")}</p> : <p>{t("user_message")}</p>}
       </div>
     </div>
   )
