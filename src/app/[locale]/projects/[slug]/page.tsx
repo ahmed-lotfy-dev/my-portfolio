23f101766fb9adf3b6bccfca99dc4caa231d5615
ahmed-lotfy-dev <elshenawy19@gmail.com>
2025-10-28 12:49:34 +0300

    fix

--- src/app/[locale]/projects/[slug]/page.tsx
+++ src/app/[locale]/projects/[slug]/page.tsx
@@ -1,12 +1,10 @@
-import { getSingleProject } from "@/src/app/actions/projectsActions";
+import { getSingleProject } from "@/src/app/actions/projectsActions"
 
-export default async function SingleProjectPage(
-  props: {
-    params: Promise<{ slug: string }>;
-  }
-) {
-  const params = await props.params;
-  const { slug } = params;
-  const { project } = await getSingleProject(slug);
-  return <div>{project?.title}</div>;
+export default async function SingleProjectPage(props: {
+  params: Promise<{ slug: string }>
+}) {
+  const params = await props.params
+  const { slug } = params
+  const { project } = await getSingleProject(slug)
+  return <div>{project?.title_en}</div>
 }
