ef4b3386ebdf9fd128a0f19a38f47c2bd1c8c443
ahmed-lotfy-dev <elshenawy19@gmail.com>
2025-12-14 12:41:12 +0200

    perf: use AVIF format for About image
    - Changed About-Image.png to About-Image.avif
    - Reduces image size from 822KB to 32KB (96% reduction)
    - AVIF provides better compression than PNG while maintaining quality

--- src/components/homepage/About.tsx
+++ src/components/homepage/About.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import myImage from "@/public/images/original-pngs/About-Image.png";
+import myImage from "@/public/images/original-pngs/About-Image.avif";
 import { getTranslations, getLocale } from "next-intl/server";
 
 export default async function About() {
