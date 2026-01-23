73a988ca915cb2b6e444af4e53480e5d83a6c274
ahmed-lotfy-dev <elshenawy19@gmail.com>
2026-01-23 18:27:43 +0200

    chore: final simplified db ssl pattern

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,8 +3,6 @@ import { Pool } from "pg"
 import * as schema from "./schema"
 
 const getSslConfig = () => {
-  // We force SSL for security, but set rejectUnauthorized to false 
-  // because we are connecting via IP and using self-signed certs.
   return { rejectUnauthorized: false };
 };
 
